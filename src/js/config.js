388f3e2411194e68533b94032bc78f00ba98f169
FireWolf240Playz <a.yordanow67@gmail.com>
2024-02-15 11:41:46 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = `91f39cee-b79d-49e8-9d6a-19f6a99ce925`;
+export const MODAL_CLOSE_SEC = 2.5;
